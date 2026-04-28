1d9f9181f87b19c3097b61f5f4ae52d06f56b9a2
MectronPrime <dasunbhagyapremathilaka@gmail.com>
2026-04-28 14:31:06 +0530

    initialize project structure,RestAPI folder structure create

---
+++ src/main/java/com/zeusglobal/products/controller/ProductController.java
@@ -0,0 +1,18 @@
+package com.zeusglobal.products.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping(value = "api/v1/")
+
+public class ProductController {
+    @GetMapping("/getProduct")
+    public String getProduct() {
+        return "get All Product";
+    }
+
+}
